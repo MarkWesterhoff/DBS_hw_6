e7105de276974e203894324202b0397d5556fb5e
root <westem@rpi.edu>
2016-04-19 16:07:46 -0700

    methods much prettier now

--- hw6.py
+++ hw6.py
@@ -19,54 +19,17 @@ NUM_ACTOR_PAGES = 2104
 NUM_MOVIE_PAGES = 4017
 
 
-# class idxReader():
-# 	def __init__(self, folder, filename, primary, ):
-# 		self.folder = folder
-# 		self.file = filename
-# 		self.fields = fields #dict
-# 		self.numpages = 0
-		
-
-# 	def foo(self):
-# 		while not done:
-# 			with open(BASE+folder+file) as f:
-# 				next_file = None
-# 				# plus one for each node it goes to
-# 				self.numpages += 1;
-# 				node = f.readline().strip()
-# 				assert node in ('internal', 'leaf')
-# 				reader = csv.reader(f, delimiter=",")
-# 				# doesn't correctly handle last line of files because it isnt in csv format
-# 				for row in reader:
-# 					print row
-# 					# Internal node
-# 					if node == 'internal':
-
-# 						if row['movieid'] >= self.movieid_low:
-# 							next_file = row['pageid']
-# 							break
-# 					elif node == 'leaf':
-# 						if row['movieid'] >= self.movieid_low and row['movieid'] <= self.movieid_high:
-# 							found = True
-# 							#if row['actorid'] >= self.actorid_low and row['actorid'] <= self.actorid_high:
-# 							actorids.append(row['actorid'])
-# 						else:
-# 							# end of block of movie ids
-# 							if found == True:
-# 								found = False
-# 								break
-
-
-
 class Query:
 	
+
 	def __init__(self, csvvals):
 		'''
-		Constructs a query
+		Constructs a Query class from a given input query
 		'''
 		assert len(csvvals) == 4
 		self.query = csvvals
 
+		# Replaces '*'s with the lowest or largest id
 		if self.query['movieid_low'] == '*':
 			self.movieid_low = MIN_MOVIE_ID
 		else:
@@ -89,26 +52,23 @@ class Query:
 
 		self.results = dict()
 
+
 	def run(self):
-		#self.results[1] = self.method_one()
-		#self.results[2] = self.method_two()
+		self.results[1] = self.method_one()
+		self.results[2] = self.method_two()
 		self.results[3] = self.method_three()
 		# return self.print_results()
 
 
-	def method_one(self):
-		'''
-		Scans movieroles_ma_idx index to find all actors,
-		 then actors_id_idx to find the names of these actors.
-		'''
-		num_pages = {'movie_idx' : 0, 'movie_table' : 0, 'actor_idx' : 0, 'actor_table' : 0}
 
-		#*********************************
-		# Going through movie_ma_idx
-		#**********************************
-		node = 'internal'
+	def get_actor_ids_from_movie_index_table(self):
+		'''
+		Goes through movie_ma_idx and returns the actor ids,
+		along the the number of index pages traversed.
+		'''
 		file = 'root.txt'
 		next_file = None
+		num_pages = 0
 		actorids = list()
 		done = False
 		found = False
@@ -117,7 +77,7 @@ class Query:
 			with open(BASE+'movieroles_ma_idx/'+file) as f:
 				next_file = None
 				# plus one for each node it goes to
-				num_pages['movie_idx'] += 1;
+				num_pages += 1;
 				node = f.readline().strip()
 				assert node in ('internal', 'leaf')
 				reader = csv.DictReader(f, delimiter=",", fieldnames = ['movieid', 'actorid', 'pageid'])
@@ -149,30 +109,33 @@ class Query:
 				done = True
 
 			file = next_file
+		return actorids, num_pages
 
-		#*********************************
-		# Going through actor_idx
-		#**********************************
+
+	def get_actor_pages_from_actor_index_table_given_actor_ids(self, actorids):
+		'''
+		Goes through actor_id_idx, given actor ids, and returns the corresponding
+		pageids (and the number of index pages traversed)
+		'''
 		
 		pageids = list()
 		
 		# Loop through each file it goes through
 		for aid in actorids:
-			node = 'internal'
 			file = 'root.txt'
 			next_file = None
+			num_pages = 0
 			done = False
 			found = False
 			while not done:
 				with open(BASE+'actors_id_idx/'+file) as f:
 					next_file = None
 					# plus one for each node it goes to
-					num_pages['actor_idx'] += 1;
+					num_pages += 1;
 					node = f.readline().strip()
 					assert node in ('internal', 'leaf')
 					reader = csv.DictReader(f, delimiter=",", fieldnames = ['id', 'pageid'])
 					# doesn't correctly handle last line of files because it isnt in csv format
-					print file
 					for row in reader:
 						# last line; move onto next file
 						if '.txt' in row['id']:
@@ -196,31 +159,96 @@ class Query:
 					done = True
 
 				file = next_file
+			return pageids, num_pages
 
 
-		#*********************************
-		# Going through actor_table
-		#**********************************
-
+	def get_actor_names_from_actor_table_given_pages(self, pageids, actorids):
+		'''
+		Goes through actor_table, given actor ids and corresponding page ids, 
+		and returns the corresponding actors (and the number of table pages traversed)
+		'''
+		num_pages = 0
 		actors = list()
 		for page, aid in zip(pageids, actorids):
 			file = 'page%s.txt' % (page);
 			with open(BASE+'actors_table/'+file) as f:
-				num_pages['actor_table'] += 1;
+				num_pages += 1;
 				reader = csv.DictReader(f, delimiter=",", fieldnames = ['atype', 'id', 'name', 'surname'])
 				for row in reader:
 					if row['id'] == aid:
 						actors.append(" ".join([row['name'], row['surname']]))
 						break
+		return actors, num_pages
+
+
+	def get_actor_names_from_actor_table_given_ids(self, actorids):
+		'''
+		Goes through actor_table, given ONLY actor ids, 
+		and returns the corresponding actors (and the number of table pages traversed)
+		'''
+		actors = list()
+		num_pages = 0
+		done = False
+		page = 1
+		while not done and page <= NUM_ACTOR_PAGES:
+			file = 'page%s.txt' % (page);
+			with open(BASE+'actors_table/'+file) as f:
+				num_pages += 1;
+				reader = csv.DictReader(f, delimiter=",", fieldnames = ['atype', 'id', 'name', 'surname'])
+				for row in reader:
+					if row['id'] in actorids:
+						actors.append(" ".join([row['name'], row['surname']]))
+						actorids.remove(row["id"])
+						if len(actorids) == 0:
+							done = True
+							break
+			page += 1
+
+		return actors, num_pages
+
+
+	def get_actor_ids_from_movie_table(self):
+		'''
+		Goes through movie_table and returns the corresponding actors 
+		(and the number of table pages traversed).
+		'''
+		actorids = list()
+		num_pages = 0
+		done = False
+		for page in xrange(1, NUM_MOVIE_PAGES+1):
+			file = 'page%s.txt' % (page);
+			with open(BASE+'movieroles_table/'+file) as f:
+				num_pages += 1;
+				reader = csv.DictReader(f, delimiter=",", fieldnames = ['actorid', 'info_1', 'info_2', 'movieid', 'role'])
+				for row in reader:
+					if ( int(row['movieid']) >= self.movieid_low and int(row['movieid']) <= self.movieid_high and 
+					int(row['actorid']) >= self.actorid_low and int(row['actorid']) <= self.actorid_high and
+					row['actorid'] not in actorids ):
+						actorids.append(row['actorid'])
+			page += 1
+
+		return actorids, num_pages
+
+
+	def method_one(self):
+		'''
+		Scans movieroles_ma_idx index to find all actors,
+		 then actors_id_idx to find the names of these actors.
+		'''
+		num_pages = {'movieroles_idx' : 0, 'movieroles_table' : 0, 'actors_idx' : 0, 'actors_table' : 0}
+
+		actorids = list()
+		actorids, num_pages['movieroles_idx'] = self.get_actor_ids_from_movie_index_table()
+
+		pageids = list()
+		pageids, num_pages['actors_idx'] = self.get_actor_pages_from_actor_index_table_given_actor_ids(actorids)
+
+		actors = list()
+		actors, num_pages['actors_table'] = self.get_actor_names_from_actor_table_given_pages(pageids, actorids)
 		
-		print actorids
-		print pageids
-		print actors
-		print num_pages
+		return actors, num_pages
 
     				
-
-
 	def method_two(self):
 		'''
 		Scans movieroles_ma_idx index to find all actors,
@@ -228,80 +256,19 @@ class Query:
 		 of these actors (if their id is in the set of actor ids found).
 		'''
 		
-		num_pages = {'movie_idx' : 0, 'movie_table' : 0, 'actor_idx' : 0, 'actor_table' : 0}
+		num_pages = {'movieroles_idx' : 0, 'movieroles_table' : 0, 'actors_idx' : 0, 'actors_table' : 0}
 
-		#*********************************
-		# Going through movie_ma_idx
-		#**********************************
-		node = 'internal'
-		file = 'root.txt'
-		next_file = None
 		actorids = list()
-		done = False
-		found = False
-		# Loop through each file it goes through
-		while not done:
-			with open(BASE+'movieroles_ma_idx/'+file) as f:
-				next_file = None
-				# plus one for each node it goes to
-				num_pages['movie_idx'] += 1;
-				node = f.readline().strip()
-				assert node in ('internal', 'leaf')
-				reader = csv.DictReader(f, delimiter=",", fieldnames = ['movieid', 'actorid', 'pageid'])
-				# doesn't correctly handle last line of files because it isnt in csv format
-				for row in reader:
-					# last line; move onto next file
-					if '.txt' in row['movieid']:
-						next_file = row['movieid']
-						break
+		actorids, num_pages['movieroles_idx'] = self.get_actor_ids_from_movie_index_table()
 
-					# Internal node
-					if node == 'internal':
-						if int(row['movieid']) >= self.movieid_low:
-							next_file = row['pageid']
-							break
-
-					# leaf node
-					elif node == 'leaf':
-						if int(row['movieid']) >= self.movieid_low and int(row['movieid']) <= self.movieid_high:
-							found = True
-							if int(row['actorid']) >= self.actorid_low and int(row['actorid']) <= self.actorid_high:
-								actorids.append(row['actorid'])
-						# end of sequential block of movie ids that fit the requirements
-						elif found == True:
-								found = False
-								break
-			# i.e. last file
-			if next_file is None:
-				done = True
-
-			file = next_file
-
-
-		#*********************************
-		# Going through actor_table
-		#**********************************
+		pageids = list()
+		pageids, num_pages['actors_idx'] = self.get_actor_pages_from_actor_index_table_given_actor_ids(actorids)
 
 		actors = list()
-		done = False
-		page = 1
-		while not done and page <= NUM_ACTOR_PAGES:
-			file = 'page%s.txt' % (page);
-			with open(BASE+'actors_table/'+file) as f:
-				num_pages['actor_table'] += 1;
-				reader = csv.DictReader(f, delimiter=",", fieldnames = ['atype', 'id', 'name', 'surname'])
-				for row in reader:
-					if row['id'] in actorids:
-						actors.append(" ".join([row['name'], row['surname']]))
-						actorids.remove(row["id"])
-						print actorids, row['id'], file
-						if len(actorids) == 0:
-							done = True
-							break
-			page += 1
+		actors, num_pages['actors_table'] = self.get_actor_names_from_actor_table_given_ids(actorids)
 
-		print actors
-		print num_pages
+		return actors, num_pages
+		
 
 	def method_three(self):
 		'''
@@ -311,48 +278,13 @@ class Query:
 		
 		num_pages = {'movie_idx' : 0, 'movie_table' : 0, 'actor_idx' : 0, 'actor_table' : 0}
 
-		#*********************************
-		# Going through movie_table
-		#**********************************
-
 		actorids = list()
-		done = False
-		for page in xrange(1, NUM_MOVIE_PAGES+1):
-			file = 'page%s.txt' % (page);
-			with open(BASE+'movieroles_table/'+file) as f:
-				num_pages['movie_table'] += 1;
-				reader = csv.DictReader(f, delimiter=",", fieldnames = ['actorid', 'info_1', 'info_2', 'movieid', 'role'])
-				for row in reader:
-					if ( int(row['movieid']) >= self.movieid_low and int(row['movieid']) <= self.movieid_high and 
-					int(row['actorid']) >= self.actorid_low and int(row['actorid']) <= self.actorid_high and
-					row['actorid'] not in actorids ):
-						actorids.append(row['actorid'])
-			page += 1
-
-		#*********************************
-		# Going through actor_table
-		#**********************************
+		actorids, num_pages['movie_table'] = self.get_actor_ids_from_movie_table()
 
 		actors = list()
-		done = False
-		page = 1
-		while not done and page <= NUM_ACTOR_PAGES:
-			file = 'page%s.txt' % (page);
-			with open(BASE+'actors_table/'+file) as f:
-				num_pages['actor_table'] += 1;
-				reader = csv.DictReader(f, delimiter=",", fieldnames = ['atype', 'id', 'name', 'surname'])
-				for row in reader:
-					if row['id'] in actorids:
-						actors.append(" ".join([row['name'], row['surname']]))
-						actorids.remove(row["id"])
-						print actorids, row['id'], file
-						if len(actorids) == 0:
-							done = True
-							break
-			page += 1
+		actors, num_pages['actors_table'] = self.get_actor_names_from_actor_table_given_ids(actorids)
 
-		print actors
-		print num_pages
+		return actors, num_pages
 
 
 	# def print_results():
