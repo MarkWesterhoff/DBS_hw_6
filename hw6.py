e259924fdc1d12f2abe31aebdc9d434db24fcaa6
root <westem@rpi.edu>
2016-04-19 13:35:19 -0700

    at least it runs

--- hw6.py
+++ hw6.py
@@ -23,73 +23,87 @@ class fileReader():
 
 
 
-class Query():
-	self.query = dict()
-	self.movieids = list()
-	self.actorids = list()
-	self.results = dict()
+class Query:
+	
 	def __init__(self, csvvals):
 		'''
 		Constructs a query
 		'''
 		assert len(csvvals) == 4
 		self.query = csvvals
+
 		if self.query['movieid_low'] == '*':
 			self.movieid_low = MIN_MOVIE_ID
-		else
+		else:
 			self.movieid_low = self.query['movieid_low']
 
 		if self.query['movieid_high'] == '*':
 			self.movieid_high = MAX_MOVIE_ID
-		else
+		else:
 			self.movieid_high = self.query['movieid_high']
 
 		if self.query['actorid_low'] == '*':
 			self.actorid_low = MIN_ACTOR_ID
-		else
+		else:
 			self.actorid_low = self.query['actorid_low']
 
 		if self.query['actorid_high'] == '*':
 			self.actorid_high = MAX_ACTOR_ID
-		else
-			self.actoridhigh = self.query['actord_high']
+		else:
+			self.actorid_high = self.query['actorid_high']
 
-		#probably a bad idea
-		self.movieids = [i for i in range(self.movieid_low, self.movieid_high+1)]
-		self.actorids = [i for i in range(self.actorid_low, self.actorid_high+1)]
+		self.results = dict()
 
-	def run():
+	def run(self):
 		self.results[1] = self.method_one()
 		# self.results[2] = self.method_two()
 		# self.results[3] = self.method_three()
 		# return self.print_results()
 
 
-	def method_one():
+	def method_one(self):
 		'''
 		Scans movieroles_ma_idx index to find all actors,
 		 then actors_id_idx to find the names of these actors.
 		'''
 		node = 'internal'
-		file = 'root.txt'
-		range_pairs = list()
-		with open(BASE+'movieroles_ma_idx/'+file) as f:
-			node = f.readline()
-			assert node in ('internal', 'leaf')
-    		reader = csv.DictReader(f, delimiter=",",fieldnames=['movieid', 'actorid', 'pageid'])
-    		last_val = 0
-    		for row in reader:
-    			#check if the node is necessary to go to
-    			for i in self.movieids:
+		next_file = 'root.txt'
+		num_pages = {'movie_idx' : 0, 'movie_table' : 0, 'actor_idx' : 0, 'actor_table' : 0}
 
-    			if row['movieid'] >= self.movieid_low and self.movieid_low >= last_val:
+		# Going through movie_ma_idx
+		actorids = list()
+		done = False
+		found = False
+		while not done:
+			with open(BASE+'movieroles_ma_idx/'+next_file) as f:
+				next_file = None
+				# 1 for each internal node
+				num_pages['movie_idx'] += 1;
+				node = f.readline().strip()
+				assert node in ('internal', 'leaf')
+				reader = csv.DictReader(f, delimiter=",",fieldnames=['movieid', 'actorid', 'pageid'])
+				# doesn't correctly handle last line of files
+				for row in reader:
+					print row
+					if node == 'internal' and row['movieid'] >= self.movieid_low:
+						next_file = row['pageid']
+						break
+					elif node == 'leaf':
+						if row['movieid'] >= self.movieid_low and row['movieid'] <= self.movieid_high:
+							found = True
+							if row['actorid'] >= self.actorid_low and row['actorid'] <= self.actorid_high:
+								actorids.append(row['actorid'])
+						else:
+							# end of block of movie ids
+							if found == True:
+								found = False
+								break
 
-    			elif row['movieid'] <= 
-    			  range_pairs.append( () )
 
-    			last_val = row['movieid']
+    				
 
-	def method_two():
+
+	def method_two(self):
 		'''
 		Scans movieroles_ma_idx index to find all actors,
 		 then scan the whole actors_table to find the names 
@@ -97,40 +111,42 @@ class Query():
 		'''
 		pass
 
-	def method_three():
+	def method_three(self):
 		'''
 		Scan both tables to answer the question; no indices.
 		 Scan movieroles_table first, then actors_table.
 		'''
 		pass
 
-	def print_results():
-		'''
-		Prints the results of a query
-		Unfinished
-		'''
-		assert self.results[1], self.results[2], self.results[3]
-		output = "Query: " + ','.join(str(self.vals['movieid_low']),
-								str(self.vals['movieid_high']),
-								str(self.vals['actorid_low']),
-								str(self.vals['actorid_high'])) + '\n'
-		output += '\n'
-		output += 'Results:\n' + [ str(i)+'\n' for i in self.results['results']]
-		output += '\n'
-		For i in range(1,4):
-			output += "Method %i total cost: %i pages\n" % (i, self.results[i]['Total'])
-					+ ""
-		return output
+	# def print_results():
+	# 	'''
+	# 	Prints the results of a query
+	# 	Unfinished
+	# 	'''
+	# 	assert self.results[1]
+	# 	assert self.results[2]
+	# 	assert self.results[3]
+	# 	output = "Query: " + ','.join(str(self.vals['movieid_low']),
+	# 							str(self.vals['movieid_high']),
+	# 							str(self.vals['actorid_low']),
+	# 							str(self.vals['actorid_high'])) + '\n'
+	# 	output += '\n'
+	# 	output += 'Results:\n' + [ str(i)+'\n' for i in self.results['results']]
+	# 	output += '\n'
+	# 	For i in range(1,4):
+	# 		output += "Method %i total cost: %i pages\n" % (i, self.results[i]['Total'])
+	# 				+ ""
+	# 	return output
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("input_file_name")
-	parser.parse_args()
-	with open(parser.input_file_name) as f:
-    	reader = csv.DictReader(f, delimiter=",",fieldnames=['movieid_low','movieid_high','actorid_low','actorid_high'])
-    	for row in reader:
-	    	q = Query(row)
-	        q.run()
-	        print q.print_results()
+	args = parser.parse_args()
+	with open(args.input_file_name) as f:
+		reader = csv.DictReader(f, delimiter=",",fieldnames=['movieid_low','movieid_high','actorid_low','actorid_high'])
+		for row in reader:
+			q = Query(row)
+			q.run()
+	        #print q.print_results()
 	        print '\n' + '*'*20 + '\n'
